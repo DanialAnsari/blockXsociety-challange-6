b9ac3334c52dc83c1070a385d0300f1f6e6672f6
Danial Ansari <danialansari998@gmail.com>
2020-11-20 18:16:50 +0500

    Add files via upload

---
+++ challange6.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.0;
+
+contract Challange6{
+    
+    uint256 public time;
+    
+       function callKeccak256(address  _add, address _add2) public pure returns(bytes32 result){
+        uint160 num =uint160(_add);
+        
+        uint160 num2=uint160(_add2);
+    
+        if(num>num2){
+            return keccak256(abi.encode(_add,_add2)) ;
+        }
+        else{
+             return keccak256(abi.encode(_add2,_add)) ;
+        }
+      
+      
+   }
+   
+    
+}
+
